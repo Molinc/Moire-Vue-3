98dd3b05c25dc2c850f0999fdd7c5a4bf52bb09d
Ilya A. Shuvalov <i.a.shuvalov@gmail.com>
2023-06-01 17:50:24 +0300

    собраны файлы к деплою: изменен publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/moire-vue3/', // ! название поддиректории, являющейся корневой на сайте, куда будет выложен проект (на гитхабе это moire-vue3)
+  publicPath: '/Moire-Vue-3/', // ! название поддиректории, являющейся корневой на сайте, куда будет выложен проект (на гитхабе это moire-vue3)
 });
