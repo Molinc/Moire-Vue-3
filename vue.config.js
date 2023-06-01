bcb0a763386e48bb8a516b10c861f1be0a3272be
Ilya A. Shuvalov <i.a.shuvalov@gmail.com>
2023-06-01 18:20:37 +0300

    очищены лишние данные, перенастроен деплой

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/Moire-Vue-3/', // ! название поддиректории, являющейся корневой на сайте, куда будет выложен проект (на гитхабе это moire-vue3)
+  // ! название поддиректории, являющейся корневой на сайте, куда будет выложен проект (на гитхабе это moire-vue3)
+  publicPath: process.env.NODE_ENV === 'production' ? '/Moire-Vue-3/' : '/',
 });
