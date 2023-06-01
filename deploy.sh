b4f79d484c2624f9dc18fcd8d3dc9599cb7dda36
Ilya A. Shuvalov <i.a.shuvalov@gmail.com>
2023-06-01 18:40:04 +0300

    сборка

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/IShuvaloff/Moire-Vue-3.git master:gh-pages
+git push -f git@github.com:IShuvaloff/Moire-Vue-3.git master:gh-pages
 
 cd -
